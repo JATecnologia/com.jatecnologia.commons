fea91cd6da51de37868e93ef001615f6ee428598
jorgefilho <jorge.filho@jatecnologia.com>
2013-11-13 14:10:57 -0200

    Modified the methods

--- jat-commons-core/src/main/java/com/jatecnologia/commons/core/encryption/EncryptionUtil.java
+++ jat-commons-core/src/main/java/com/jatecnologia/commons/core/encryption/EncryptionUtil.java
@@ -1,6 +1,5 @@
 package com.jatecnologia.commons.core.encryption;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -10,19 +9,32 @@ public class EncryptionUtil {
 	public static final String SHA1 = "SHA-1";
 	public static final String SHA256 = "SHA-256";
 	
-	public static String generatePasswordSHA256(String password) {		 
+	public static String generateHash(String value, String algorithm) {
+		byte[] bytes = generateByteHash(value, algorithm);
+		
+		return convertArray(bytes);
+	}
+
+	private static byte[] generateByteHash(String value, String algorithm) {
 		try {
-			MessageDigest algorithm = MessageDigest.getInstance(SHA256);
-						
-			algorithm.update(password.getBytes());                  
-           
-			String encryptedPassword = new BigInteger(1, algorithm.digest()).toString(16);
-            
-			return encryptedPassword;
-			
-		} catch (NoSuchAlgorithmException e) {			
+			MessageDigest md = MessageDigest.getInstance(algorithm);
+			md.update(value.getBytes());
+			return md.digest();
+		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
-		}		
-		return password;
+			return null;
+		}
+	}
+
+	private static String convertArray(byte[] bytes) {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < bytes.length; i++) {
+			int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+			int parteBaixa = bytes[i] & 0xf;
+			if (parteAlta == 0)
+				s.append('0');
+			s.append(Integer.toHexString(parteAlta | parteBaixa));
+		}
+		return s.toString();
 	}
 }
